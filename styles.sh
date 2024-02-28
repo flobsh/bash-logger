d38e46762811d2d0b48f004b88d77007d58a350d
Florian Bescher <florian.bescher22@gmail.com>
2024-02-28 20:51:24 +0100

    feat[formatting]: add a format field in the stylesheet

--- styles.sh
+++ styles.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+. "$(dirname "${BASH_SOURCE[0]}")"/formatting.sh
+
 declare -r -x -A ERROR=(
   [prefix]="error: "
+  [format]="${RED}"
 )
 
 declare -r -x -A WARNING=(
   [prefix]="warning: "
+  [format]="${YELLOW}"
 )
 
 declare -r -x -A INFO=(
   [prefix]="info: "
+  [format]="${WHITE}"
 )
 
 declare -r -x -A DEBUG=(
   [prefix]="debug: "
+  [format]="${BLACK}"
 )
