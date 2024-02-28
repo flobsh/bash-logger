636051d3b74b16b8efaa692385b927a174847db9
Florian Bescher <florian.bescher22@gmail.com>
2024-02-28 20:50:50 +0100

    feat[formatting]: add ANSI formatting codes

---
+++ formatting.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Formatting
+# shellcheck disable=SC2034
+{
+  RESET="$(tput sgr0)"
+  BOLD="$(tput bold)"
+  BLINK="$(tput blink)"
+  ULINE="$(tput smul)"
+  DIM="$(tput dim)"
+  ITALIC="$(tput sitm)"
+}
+
+# Colors
+# shellcheck disable=SC2034
+{
+  BLACK="$(tput setaf 0)"
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  MAGENTA="$(tput setaf 5)"
+  CYAN="$(tput setaf 6)"
+  WHITE="$(tput setaf 7)"
+}
