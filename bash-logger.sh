775ae87595b9b7673841fd19774c9627e6f116e8
Florian Bescher <florian.bescher22@gmail.com>
2024-03-09 22:14:49 +0100

    feat: add dependency presence check

--- bash-logger.sh
+++ bash-logger.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
-# Global options
+# DEPENDENCIES
+FORMATSHEET_PATH="$(dirname "${BASH_SOURCE[0]}")/formatting.conf"
+STYLESHEET_PATH="$(dirname "${BASH_SOURCE[0]}")/styles.conf"
+
+# Check dependencies presence
+_check_dependencies() {
+  if ! [[ -f "${FORMATSHEET_PATH}" ]]; then
+    printf "error: format sheet not found, expected path is %s" "${FORMATSHEET_PATH}" >&2
+    exit 1
+  fi
+  if ! [[ -f "${STYLESHEET_PATH}" ]]; then
+    printf "error: stylesheet not found, expected path is %s" "${STYLESHEET_PATH}" >&2
+    exit 1
+  fi
+}
+
+_check_dependencies
+
+# GLOBAL OPTIONS
 # LOG_LEVEL sets the level of log the script will output.
 # The logger will display the messages for LOG_LEVEL and higher.
 # Values can be DEBUG, INFO, WARNING or ERROR (default INFO).
 LOG_LEVEL=${LOG_LEVEL:-INFO}
+
 # LOG_SCRIPT_NAME enables display of the script issuing the log.
-# Values can be (default DEBUG):
-# - OFF: the logger will never display the script's name
-# - DEBUG, INFO, WARNING or ERROR: the logger will display the script's name only for this log level and higher
-# - ON: the logger will always display the script's name
+# Values can be (default true):
+# - true: the logger will display the script's name if the style
+#         allows it with the [script-name]=true key.
+# - false: the logger will never display the script's name
 LOG_SCRIPT_NAME=${LOG_SCRIPT_NAME:-false}
+
 # LOG_FORMATTING enables output colors and text formatting
-# Values can be ON or OFF (default ON)
+# Values can be true or false (default true)
 LOG_FORMATTING=${LOG_FORMATTING:-true}
 
 # Source configuration files
 if [[ "${LOG_FORMATTING}" = true ]]; then
-  . "$(dirname "${BASH_SOURCE[0]}")"/formatting.conf
+  . "${FORMATSHEET_PATH}"
 fi
-. "$(dirname "${BASH_SOURCE[0]}")"/styles.conf
+. "${STYLESHEET_PATH}"
 
 declare -r -A LOG_LEVELS=(
   [ANY]=0
@@ -43,8 +63,6 @@ _log() {
 
   # Local variables
   local script_name=""
-  local formatting=""
-  local format_reset=""
 
   if [[ "${LOG_LEVELS[${style[log_level]:-ANY}]}" -gt "${LOG_LEVELS[${LOG_LEVEL}]}" ]]; then
     return 0
@@ -81,3 +99,5 @@ log_warn() {
 log_err() {
   _log ERROR "${1}"
 }
+
+_check_dependencies
